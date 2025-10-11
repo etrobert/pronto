4fc46f905b4fdbef60e7be3c1d6ecc2cda0aaf5f
Etienne Robert <etiennerobert33@gmail.com>
2025-10-11 03:59:39 +0200

    Display home directory as ~ in output path

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
-use std::env;
+use std::{env, path::PathBuf};
 
 fn main() {
-    let path = env::current_dir().unwrap();
-    println!("{}", path.display());
+    let path = env::current_dir().expect("Could not fetch current directory");
+
+    let home = env::var_os("HOME").expect("HOME environment variable is not defined");
+
+    let home_path = PathBuf::from(&home);
+
+    match path.strip_prefix(home_path) {
+        Ok(rest) if rest.as_os_str().is_empty() => {
+            println!("~");
+        }
+        Ok(rest) => {
+            println!("~/{}", rest.display());
+        }
+        _ => {
+            println!("{}", path.display());
+        }
+    }
 }
