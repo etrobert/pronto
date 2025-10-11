032a8ac8579c752f5fff1c799e9bc3b6eb02d5bf
Etienne Robert <etiennerobert33@gmail.com>
2025-10-11 04:14:10 +0200

    Refactor path logic into get_path function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{env, path::PathBuf};
 
-fn main() {
+fn get_path() -> String {
     let path = env::current_dir().expect("Could not fetch current directory");
 
     let home = env::var_os("HOME").expect("HOME environment variable is not defined");
@@ -8,14 +8,14 @@ fn main() {
     let home_path = PathBuf::from(&home);
 
     match path.strip_prefix(home_path) {
-        Ok(rest) if rest.as_os_str().is_empty() => {
-            println!("~");
-        }
-        Ok(rest) => {
-            println!("~/{}", rest.display());
-        }
-        _ => {
-            println!("{}", path.display());
-        }
+        Ok(rest) if rest.as_os_str().is_empty() => "~".to_string(),
+        Ok(rest) => format!("~/{}", rest.display()),
+        _ => path.display().to_string(),
     }
+}
+
+fn main() {
+    let path = get_path();
+
+    println!("{}", path);
 }
